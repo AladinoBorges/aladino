a037821aa6d633d107f70b40d6dd41c2506073f7
Aladino Borges <aladino@headhunters.digital>
2024-03-17 14:02:48 +0100

    chore[app]: layout
    - imports default created css file that handles bulma css styles.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '../css/bulma.css';
-import "./globals.css";
+import '../bulma/global.css';
+// import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
