f7b2b251f7a2b15e90d8bd541a3a9ad014038ab2
Aladino Borges <aladino@headhunters.digital>
2024-03-17 15:57:34 +0100

    feat[app]: homepage
    - implements temporary homepage hero section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+import { alborsui } from '@/components/alborsui';
+
 export default function Home() {
   return (
     <main>
-      <section className="hero is-medium is-info">
+      <section className="hero is-medium is-primary">
       <div className='hero-body'>
-        <h1 className="title"><span>aladino</span> <span>borges</span></h1>
+        <h1 className="title is-1"><span>aladino</span> <span>borges</span></h1>
 
-        <p className="subtitle"><span>software engineer</span> and <span>fullstack web developer</span></p>
+        <p className="subtitle is-3">
+          <span>engenheiro de software</span>, <span>desenvolvedor web fullstack</span> e <span>minimalista</span>
+        </p>
 
-        <button className="button is-dark is-outlined is-rounded" type="button">say hello there!</button>
+        <alborsui.Button category='dark' className="is-outlined is-rounded" type="button">
+          {`diga "olá mundo"`}
+        </alborsui.Button>
       </div>
       </section>
     </main>
