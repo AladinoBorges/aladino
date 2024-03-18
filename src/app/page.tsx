5e6675ea32d5a70464fdee62aba8a21434ffce08
¨Aladino <aladino@headhunters.digital>
2024-03-18 07:29:12 -0300

    refactor[app]: home page
    - integrates hero header organism.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
+"use client";
 import { alborsui } from '@/components/alborsui';
 
 export default function Home() {
   return (
     <main>
-      <section className="hero is-medium is-primary">
-      <div className='hero-body'>
-        <h1 className="title is-1"><span>aladino</span> <span>borges</span></h1>
-
-        <p className="subtitle is-3">
-          <span>engenheiro de software</span>, <span>desenvolvedor web fullstack</span> e <span>minimalista</span>
-        </p>
-
-        <alborsui.Button category='dark' className="is-outlined is-rounded" type="button">
-          {`diga "olá mundo"`}
-        </alborsui.Button>
-      </div>
-      </section>
+      <alborsui.HeroHeader title="aladino borges" subtitle="engenheiro de software, desenvolvedor web fullstack  e minimalista" callToActionClick={() => {}} callToActionText={`diga "olá mundo"`}  />
     </main>
   );
 }
