5ae618316a6d293a2ecf3a4b8433cf9f2e2b9ac3
¨Aladino <aladino@headhunters.digital>
2024-03-20 16:08:19 -0300

    chore[components]: organisms - hero header
    - formats file with prettier.

--- src/components/organisms/HeroHeader.tsx
+++ src/components/organisms/HeroHeader.tsx
@@ -4,26 +4,31 @@ interface HeroPropTypes {
   title: string;
   subtitle: string;
   callToActionText: string;
-  callToActionClick: () => any
-};
+  callToActionClick: () => any;
+}
 
-export const HeroHeader = ({ title, subtitle, callToActionText, callToActionClick }: HeroPropTypes) => {
+export const HeroHeader = ({
+  title,
+  subtitle,
+  callToActionText,
+  callToActionClick,
+}: HeroPropTypes) => {
   return (
-    <section className="hero is-medium is-primary">
+    <section className='hero is-medium is-primary'>
       <div className='hero-body'>
-        <h1 className="title is-1">{title}</h1>
+        <h1 className='title is-1'>{title}</h1>
 
-        <p className="subtitle is-3">{subtitle}</p>
+        <p className='subtitle is-3'>{subtitle}</p>
 
         <Button
-          type="button"
+          type='button'
           category='dark'
           onClick={callToActionClick}
-          className="is-outlined is-rounded"
+          className='is-outlined is-rounded'
         >
           {callToActionText}
         </Button>
       </div>
-      </section>
+    </section>
   );
 };
