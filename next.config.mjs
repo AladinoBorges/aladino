237f3bac903cf8399ade469ec0d897781cf77bfd
¨Aladino <aladino@headhunters.digital>
2024-03-20 16:01:46 -0300

    chore[app]: configures external images sources on next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'bulma.io',
+      port: '',
+      pathname: '/images/**'
+    }]
+  }
+};
 
 export default nextConfig;
