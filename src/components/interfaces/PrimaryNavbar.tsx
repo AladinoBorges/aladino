aafa6dd78ebdaf3532197292a4ee92224fcc123d
¨Aladino <aladino@headhunters.digital>
2024-03-20 15:57:47 -0300

    feat[app]: interfaces
    - implements default top navigation bar.

---
+++ src/components/interfaces/PrimaryNavbar.tsx
@@ -0,0 +1,33 @@
+import { NabBar } from '../alborsui';
+
+export const PrimaryNavBar = () => {
+  const navigationItems = [
+    { label: 'home', link: '/', type: '', priority: '' },
+    { label: 'sobre', link: '/about', type: '', priority: '' },
+    { label: 'projetos', link: 'projects', type: '', priority: '' },
+    { label: 'empresas', link: 'companies', type: '', priority: '' },
+    { label: 'contacto', link: 'contact', type: '', priority: '' },
+  ];
+
+  const logotype = {
+    pathTo: '/',
+    alternativeText: 'logomarca de aladino borges',
+    sourceUrl: 'https://bulma.io/images/bulma-logo.png',
+  };
+
+  const navigationButtons = [
+    { label: 'entrar', link: '/login', type: 'button', priority: 'primary' },
+    { label: 'registrar', link: '/registration', type: 'button', priority: 'dark' },
+  ];
+
+  return (
+    <NabBar
+      type='light'
+      withLogotype
+      position='top'
+      logotype={logotype}
+      navigationItems={navigationItems}
+      navigationButtons={navigationButtons}
+    />
+  );
+};
